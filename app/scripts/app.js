e44a05f9b60c0327d386c59d76d96e064f0470d3
why <wcl6@sfu.ca>
2015-01-25 13:36:34 -0800

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name selectorE
+ * @description
+ * # selectorE
+ *
+ * Main module of the application.
+ */
+angular
+  .module('selectorE', [
+    'ngAnimate',
+    'ngMessages',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'CardListCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
